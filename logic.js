4ae7eb661714e128d0ecff5ad70935fcb3a45dae
shades9a <81335214+shades9a@users.noreply.github.com>
2021-07-24 10:54:10 -0500

    Update logic.js

--- logic.js
+++ logic.js
@@ -77,6 +77,7 @@ function createMap(earthquakes) {
 
   legend.onAdd = function () {
   
+    
       var div = L.DomUtil.create('div', 'info legend'),
           magnitudes = [0, 1, 2, 3, 4, 5];
   
